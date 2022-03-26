0fcb3a3aca6f80c08c4b9ac335760ca8a1073c71
ccastro <caiofcastro@usp.br>
2022-03-26 17:21:48 -0300

    add strategies

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +18,55 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
-direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+direction_t last_direction_a = DIR_DOWN_LEFT;
+position_t *last_position_a = NULL;
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+direction_t new_direction_a()
+{
+  int r = rand();
+  switch (r % 8)
+  {
+  case 0:
+    return (direction_t)DIR_UP;
+  case 1:
+    return (direction_t)DIR_UP_RIGHT;
+  case 2:
+    return (direction_t)DIR_RIGHT;
+  case 3:
+    return (direction_t)DIR_DOWN_RIGHT;
+  case 4:
+    return (direction_t)DIR_DOWN;
+  case 5:
+    return (direction_t)DIR_DOWN_LEFT;
+  case 6:
+    return (direction_t)DIR_LEFT;
+  default:
+    return (direction_t)DIR_UP_LEFT;
+  }
 }
 
+direction_t execute_attacker_strategy(
+    position_t attacker_position, Spy defender_spy)
+{
+
+  if (last_position_a == NULL)
+  {
+    last_position_a = malloc(sizeof(struct position *));
+    last_position_a->i = ULLONG_MAX;
+    last_position_a->j = ULLONG_MAX;
+    srand(time(NULL));
+  }
+
+  if (equal_positions(*last_position_a, attacker_position))
+  {
+    last_direction_a = new_direction_a();
+  }
+
+  last_position_a->i = attacker_position.i;
+  last_position_a->j = attacker_position.j;
+
+  UNUSED(defender_spy);
+
+  return last_direction_a;
+}
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,7 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +18,56 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+direction_t last_direction_d = DIR_DOWN_LEFT;
+position_t *last_position_d = NULL;
+
+direction_t new_direction_d()
+{
+  int r = rand();
+  switch (r % 8)
+  {
+  case 0:
+    return (direction_t)DIR_UP;
+  case 1:
+    return (direction_t)DIR_UP_RIGHT;
+  case 2:
+    return (direction_t)DIR_RIGHT;
+  case 3:
+    return (direction_t)DIR_DOWN_RIGHT;
+  case 4:
+    return (direction_t)DIR_DOWN;
+  case 5:
+    return (direction_t)DIR_DOWN_LEFT;
+  case 6:
+    return (direction_t)DIR_LEFT;
+  default:
+    return (direction_t)DIR_UP_LEFT;
+  }
+}
+
 direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
+    position_t defender_position, Spy attacker_spy)
+{
+
+  if (last_position_d == NULL)
+  {
+    last_position_d = malloc(sizeof(struct position *));
+    last_position_d->i = ULLONG_MAX;
+    last_position_d->j = ULLONG_MAX;
+    srand(time(NULL));
+  }
+
+  if (equal_positions(*last_position_d, defender_position))
+  {
+    last_direction_d = new_direction_d();
+  }
+
+  last_position_d->i = defender_position.i;
+  last_position_d->j = defender_position.j;
+
   UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  return last_direction_d;
 }
 
 /*----------------------------------------------------------------------------*/
